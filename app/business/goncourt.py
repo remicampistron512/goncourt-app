657fc4220ad67d737a4551dd6ecf5e68ae0ef329
Rémi Campistron <remi.campistron@gmail.com>
2025-12-10 18:11:19 +0100

    Création de la méthode set_vote_for_book dans Goncourt.py, fixes #14

--- app/business/goncourt.py
+++ app/business/goncourt.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 
 from daos.book_dao import BookDao
 from daos.phase_dao import PhaseDao
+from daos.vote_dao import VoteDao
 
 
 @dataclass
@@ -32,5 +33,9 @@ class Goncourt:
     @staticmethod
     def set_books_for_phase(phase_id: int, book_ids: list[int]):
         phase_dao: PhaseDao = PhaseDao()
-        phase_dao.set_books_for_phase(phase_id,book_ids)
+        phase_dao.set_books_for_phase(phase_id, book_ids)
 
+    @staticmethod
+    def set_vote_for_book(id_jury_member: int, id_phase: int, id_book: int):
+        vote_dao: VoteDao = VoteDao()
+        vote_dao.set_votes_for_book(id_jury_member, id_phase, id_book)
