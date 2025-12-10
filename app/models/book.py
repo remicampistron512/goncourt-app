245133cdbacf6e2c494369125d48008aa1c8e099
Rémi Campistron <remi.campistron@gmail.com>
2025-12-10 12:56:00 +0100

    Création de classe Book dans Models. fixes #8

---
+++ app/models/book.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+"""
+Classe Book
+"""
+import datetime
+import decimal
+from dataclasses import dataclass, field
+from typing import Optional
+
+
+@dataclass
+class Book:
+    """Adresse d'une personne (enseignant ou élève)."""
+    id_book: Optional[int] = field(default=None, init=False)
+    book_title: str
+    summary: str
+    publishing_date: datetime.date
+    nb_pages: int
+    isbn: str
+    editor_price: decimal.Decimal
+
+    def __str__(self) -> str:
+        book_id = self.id if self.id is not None else "?"
+        return (
+            f"[Book #{book_id}] "
+            f"'{self.book_title}' "
+            f"({self.publishing_date:%Y-%m-%d}) - "
+            f"{self.nb_pages} pages - "
+            f"ISBN {self.isbn} - "
+            f"{self.editor_price} €"
+        )
