4ad7f41afc350736407054fdacccbec0b0727d12
Rémi Campistron <remi.campistron@gmail.com>
2025-12-10 15:44:43 +0100

    Ajout de l'affichage/ajout des livres selon la phase dans main

--- app/main.py
+++ app/main.py
@@ -20,6 +20,26 @@ def main() -> None:
         print(book)
     print("")
 
+    """print("----- AJOUT DE LIVRES A LA PHASE 2 -----")
+    book_id_list = [1,2,3,4,5,6,7,8]
+    goncourt.set_books_for_phase(2,book_id_list)
+
+    print("----- AJOUT DE LIVRES A LA PHASE 3 -----")
+    book_id_list2 = [1,2,3,4]
+    goncourt.set_books_for_phase(3,book_id_list2)"""
+
+    print("----- AFFICHAGE DES LIVRES DE LA PHASE 2 -----")
+    books2 = goncourt.get_all_books_by_phase(2)
+    for book in books2:
+        print(book)
+    print("")
+
+    print("----- AFFICHAGE DES LIVRES DE LA PHASE 3 -----")
+    books3 = goncourt.get_all_books_by_phase(3)
+    for book in books3:
+        print(book)
+    print("")
+
 
 if __name__ == '__main__':
     main()
