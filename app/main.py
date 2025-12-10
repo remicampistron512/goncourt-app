40be03e1507680c6dabc5896d8f72e8fd44f448a
Rémi Campistron <remi.campistron@gmail.com>
2025-12-10 12:57:54 +0100

    Ajout de la fonction temporaire d'affichage dans main. fixes #6

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+from business.goncourt import Goncourt
+
 def main() -> None:
     """Programme principal."""
     print("""\
@@ -5,6 +7,12 @@ def main() -> None:
     Bienvenue dans l'application Goncourt
     --------------------------""")
 
+    goncourt : Goncourt = Goncourt()
+    print("----- AFFICHAGE DE TOUS LES LIVRES-----")
+    books = goncourt.get_all_books()
+    for book in books:
+        print(book)
+    print("")
 
 if __name__ == '__main__':
     main()
