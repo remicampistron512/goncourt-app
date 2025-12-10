dfeb745361a8ed564c207ea7c038f6bc0b8f61a9
Rémi Campistron <remi.campistron@gmail.com>
2025-12-10 10:24:51 +0100

    Création du fichier Main.py. fixes #1

---
+++ app/main.py
@@ -0,0 +1,10 @@
+def main() -> None:
+    """Programme principal."""
+    print("""\
+    --------------------------
+    Bienvenue dans l'application Goncourt
+    --------------------------""")
+
+
+if __name__ == '__main__':
+    main()
